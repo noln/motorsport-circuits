eb82dfa9c672531c9d3f1e3210bebe3e62d1979c
Matt Fenlon <matt@jammy.io>
2015-12-31 18:23:24 +0100

    Created CircuitItem to allow serialisation

---
+++ app/src/main/java/com/mattfenlon/osmap/POJO/CircuitItem.java
@@ -0,0 +1,48 @@
+package com.mattfenlon.osmap.POJO;
+
+
+/**
+ * Created by Matt Fenlon on 31/12/15.
+ */
+public class CircuitItem {
+
+    int lat, lon;
+    String name;
+
+    public CircuitItem(int lat, int lon, String name) {
+
+        this.lat = lat;
+        this.lon = lon;
+        this.name = name;
+    }
+
+    public int getLat() {
+
+        return lat;
+    }
+
+    public void setLat(int lat) {
+
+        this.lat = lat;
+    }
+
+    public int getLon() {
+
+        return lon;
+    }
+
+    public void setLon(int lon) {
+
+        this.lon = lon;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+}
